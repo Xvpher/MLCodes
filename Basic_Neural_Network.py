a841d354c753866ee0f50586025d85639d6b196d
Xvpher <sshadyslim313@gmail.com>
2019-03-30 11:57:32 +0530

    first commit

---
+++ Basic_Neural_Network.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+
+def CreateModel():
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    X_train = tf.keras.utils.normalize(x_train, axis=1)
+    X_test = tf.keras.utils.normalize(x_test, axis=1)
+
+    model = tf.keras.models.Sequential()
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", mertrics=["accuracy"])
+
+    model.fit(X_train, y_train, epochs=3)
+    model.save("Basic_Neural_Network.model")
+
+if not(os.path.exists("Basic_Neural_Network.model")):
+    CreateModel()
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+X_train = tf.keras.utils.normalize(x_train, axis=1)
+X_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.load_model("Basic_Neural_Network.model")
+print (model.evaluate(X_test, y_test , verbose=1))
+predictions = model.predict([X_test])
+for i in range(10):
+    print (np.argmax(predictions[i]))
+
+for i in range(10):
+    plt.subplot(2,5,i+1)
+    plt.imshow(x_test[i])
+plt.show()
